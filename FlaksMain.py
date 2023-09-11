48387e477e5fda37384b40ceb708f1cd67fe9982
ArtemGolovatyuk <artem.golovatyuk.2005@gmail.com>
2023-09-11 11:49:33 +0300

    rework characterCreation , add rework FlaskMain

--- FlaksMain.py
+++ FlaksMain.py
@@ -1,24 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for
-from characterCreation import Character
+from characterCreation import Character, character1, character2
 
 
 app = Flask(__name__, template_folder="teamplates")
 
 
-class Character:
-    def __init__(self):
-        self.health = 100  # Здоровье от 0 до 100
-        self.attack = 0  # Урон от 0 до 20
+def get_winner():
+    if character1.health <= 0 and character2.health <= 0:
+        return "Ничья"
+    elif character1.health <= 0:
+        return character2.name
+    elif character2.health <= 0:
+        return character1.name
 
-    def take_damage(self, damage):
-        self.health -= damage
-
-    def reset_attack(self):
-        import random
-        self.attack = random.randint(1, 20)
-
-character1 = Character()
-character2 = Character()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -27,22 +21,10 @@ def index():
         character2.take_damage(character1.attack)
         character1.reset_attack()
         character2.reset_attack()
-        
-
         if character1.health <= 0 or character2.health <= 0:
-            print("Персонаж 1" if character1.health > character2.health else "Персонаж 2")
-            winner = "Персонаж 1" if character1.health > character2.health else "Персонаж 2"
+            winner = get_winner()
             return render_template('winner.html', winner=winner)
-
-
     return render_template('index.html', character1=character1, character2=character2)
-def get_winner():
-    if character1.health <= 0 and character2.health <= 0:
-        return "Ничья"
-    elif character1.health <= 0:
-        return "Персонаж 2"
-    elif character2.health <= 0:
-        return "Персонаж 1"
 
 
 if __name__ == "__main__":
--- characterCreation.py
+++ characterCreation.py
@@ -2,7 +2,7 @@ import random
 from faker import Faker
 
 class Character:
-    def __init__(self, name):
+    def __init__(self):
         fake = Faker()
         self.name = fake.name()
         self.critical_attack = random.randint(1, 10)
@@ -11,5 +11,19 @@ class Character:
         self.attack = random.randint(5, 20)
         self.luck = random.randint(1, 10)
 
+
+    def take_damage(self, damage):
+        actual_damage = max(damage - self.armor, 0)
+        if random.randint(1, 10) <= self.critical_attack:
+            actual_damage *= 2
+        self.health -= actual_damage
+
+    def reset_attack(self):
+        self.attack = random.randint(1, 20)
+        
     def __str__(self):
         return f"{self.name}: критическая атака {self.critical_attack}, здоровье {self.health}, броня {self.armor}, атака {self.attack}, удача {self.luck}"
+    
+
+character1 = Character()
+character2 = Character()
