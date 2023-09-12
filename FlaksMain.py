a40b549a62bde92710267b3450cf12ee7022a12a
ArtemGolovatyuk <artem.golovatyuk.2005@gmail.com>
2023-09-12 21:51:50 +0300

    pep8

--- FlaksMain.py
+++ FlaksMain.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for
-from characterCreation import Character, character1, character2, Player, Playeryou
+from flask import Flask, render_template, request
 
+from characterCreation import character1, character2, Player_you
 
-app = Flask(__name__, template_folder="teamplates")
+app = Flask(__name__, template_folder="templates")
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -10,30 +10,30 @@ def index():
     if request.method == 'POST':
         if 'c1damage' in request.form:
             if request.form['c1damage'] == 'damage':
-                character1.take_damage(Playeryou.yourattack)
-                Playeryou.take_damage(character1.attack)
-                Playeryou.take_damage(character2.attack)
+                character1.take_damage(Player_you.your_attack)
+                Player_you.take_damage(character1.attack)
+                Player_you.take_damage(character2.attack)
                 character1.reset_attack()
                 character2.reset_attack()
-                Playeryou.reset_attackyour()
+                Player_you.reset_your_attack()
 
         elif 'c2damage' in request.form:
             if request.form['c2damage'] == 'damage2':
-                character2.take_damage(Playeryou.yourattack)
-                Playeryou.take_damage(character1.attack)
-                Playeryou.take_damage(character2.attack)
+                character2.take_damage(Player_you.your_attack)
+                Player_you.take_damage(character1.attack)
+                Player_you.take_damage(character2.attack)
                 character1.reset_attack()
                 character2.reset_attack()
-                Playeryou.reset_attackyour()
+                Player_you.reset_your_attack()
 
         if character1.health <= 0:
             character1.alive = False
         if character2.health <= 0:
             character1.alive = False
-        if Playeryou.health <= 0:
+        if Player_you.health <= 0:
             return render_template('winner.html')
 
-    return render_template('index.html', character1=character1, character2=character2, Playeryou=Playeryou)
+    return render_template('index.html', character1=character1, character2=character2, Player_you=Player_you)
 
 
 if __name__ == "__main__":
--- characterCreation.py
+++ characterCreation.py
@@ -1,21 +1,23 @@
 import random
 from faker import Faker
 
+
 class Player:
     def __init__(self):
         self.health = 100
-        self.yourarmor = random.randint(1, 10)
-        self.yourattack = random.randint(10, 20)
+        self.your_armor = random.randint(1, 10)
+        self.your_attack = random.randint(10, 20)
 
     def take_damage(self, damage):
-        actual_damage = max(damage - self.yourarmor, 0)
+        actual_damage = max(damage - self.your_armor, 0)
         self.health -= actual_damage
 
-    def reset_attackyour(self):
-        self.yourattack = random.randint(1, 20)
+    def reset_your_attack(self):
+        self.your_attack = random.randint(1, 20)
 
 
-Playeryou = Player()
+Player_you = Player()
+
 
 class Character:
     def __init__(self):
@@ -27,21 +29,18 @@ class Character:
         self.attack = random.randint(5, 20)
         self.luck = random.randint(1, 10)
         self.alive = True
-        
 
-    
     def take_damage(self, damage):
         actual_damage = max(damage - self.armor, 0)
         self.health -= actual_damage
 
     def reset_attack(self):
         self.attack = random.randint(1, 20)
-        
+
     def __str__(self):
-        return f"{self.name}: критическая атака {self.critical_attack}, здоровье {self.health}, броня {self.armor}, атака {self.attack}, удача {self.luck}"
-    
+        return (f"{self.name}: критическая атака {self.critical_attack}, здоровье {self.health}, броня {self.armor}, "
+                f"атака {self.attack}, удача {self.luck}")
+
 
 character1 = Character()
 character2 = Character()
-    
-
