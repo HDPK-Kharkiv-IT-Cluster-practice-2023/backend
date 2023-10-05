8ee3c848dcfcd66be860336417d1914b440a53ec
tetyaZyna <sv2408sv@gmail.com>
2023-10-05 17:22:12 +0200

    Add pythonanywhere

--- console/repository/CharactersDB.py
+++ console/repository/CharactersDB.py
@@ -1,123 +1,213 @@
 import psycopg2
 from psycopg2 import extras
+from sshtunnel import SSHTunnelForwarder
 
 
 class CharacterRepository:
     def __init__(self):
         self.connection_creds = {
-            'host': 'localhost',
-            'database': 'charactersdb',
-            'user': 'postgres',
-            'password': 'admin'
+            'host': 'bandydan-3203.postgres.pythonanywhere-services.com',
+            'port': 13203,
+            'db_username': 'super',
+            'db_password': 'U6Tdw8ReM',
+            'database_name': 'charactersdb',
+            'ssh_host': 'ssh.pythonanywhere.com',
+            'ssh_port': 22,
+            'ssh_username': 'bandydan',
+            'ssh_password': 'xb6W7LHNJ6!cRKi',
+            'ssh_private_key_password': 'masterkey',
+            'ssh_private_key': '/home/vitaly/.ssh/id_rsa'
         }
 
-    def _create_connection(self):
-        return psycopg2.connect(
-            host=self.connection_creds.get('host'),
-            database=self.connection_creds.get('database'),
-            user=self.connection_creds.get('user'),
-            password=self.connection_creds.get('password')
+        # local database
+
+        # self.connection_creds = {
+        #     'host': 'localhost',
+        #     'database': 'charactersdb',
+        #     'user': 'postgres',
+        #     'password': 'admin'
+        # }
+
+    # local database
+
+    # def _create_connection(self, tunnel):
+    #     # return psycopg2.connect(
+    #     #     host=self.connection_creds.get('host'),
+    #     #     database=self.connection_creds.get('database'),
+    #     #     user=self.connection_creds.get('user'),
+    #     #     password=self.connection_creds.get('password')
+    #     # )
+
+    # def _create_tunnel(self):
+    #     return None
+
+    def _create_tunnel(self):
+        return SSHTunnelForwarder(
+                (self.connection_creds.get('ssh_host'), self.connection_creds.get('ssh_port')),
+                ssh_username=self.connection_creds.get('ssh_username'),
+                ssh_password=self.connection_creds.get('ssh_password'),
+                ssh_private_key=self.connection_creds.get('ssh_private_key'),
+                ssh_private_key_password=self.connection_creds.get('ssh_private_key_password'),
+                remote_bind_address=(self.connection_creds.get('host'), self.connection_creds.get('port'))
         )
+
+    def _create_connection(self, tunnel):
+        connection = psycopg2.connect(
+            user=self.connection_creds.get('db_username'),
+            password=self.connection_creds.get('db_password'),
+            host='127.0.0.1',
+            port=tunnel.local_bind_port,
+            database=self.connection_creds.get('database_name'),
+        )
+        return connection
 
     def _create_in_database(self, character):
-        connection = self._create_connection()
+        tunnel = self._create_tunnel()
+        tunnel.start()
+        connection = self._create_connection(tunnel)
         cursor = connection.cursor()
-        cursor.execute(
-            "INSERT INTO characters (name, level, xp, max_health, health, armor, attack, luck, balance, alive, "
-            "critical_attack, playability, stat_points)"
-            "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) RETURNING id",
-            (character.name, character.level, character.xp, character.max_health, character.health, character.armor,
-             character.attack, character.luck, character.balance, character.alive, character.critical_attack,
-             character.playability, character.stat_points)
-        )
-        new_id = cursor.fetchone()[0]
-        connection.commit()
-        cursor.close()
-        connection.close()
-        return new_id
-
-    def _update_stats(self, character):
-        connection = self._create_connection()
-        cursor = connection.cursor()
-        cursor.execute(
-            "UPDATE characters SET name = %s, level = %s, xp = %s, max_health = %s, health = %s , armor = %s, "
-            "attack = %s, luck = %s, balance = %s, alive = %s, critical_attack = %s, playability = %s, "
-            "stat_points = %s WHERE id = %s",
-            (character.name, character.level, character.xp, character.max_health, character.health, character.armor,
-             character.attack, character.luck, character.balance, character.alive, character.critical_attack,
-             character.playability, character.stat_points, character.id)
-        )
-        connection.commit()
-        cursor.close()
-        connection.close()
-
-    def add_all(self, characters_list):
-        connection = self._create_connection()
-        cursor = connection.cursor()
-        for character in characters_list:
+        try:
             cursor.execute(
                 "INSERT INTO characters (name, level, xp, max_health, health, armor, attack, luck, balance, alive, "
-                "critical_attack, playability, stat_points)"
-                "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) RETURNING id",
+                "critical_attack, playability, stat_points, owner, status)"
+                "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) RETURNING id",
                 (character.name, character.level, character.xp, character.max_health, character.health, character.armor,
                  character.attack, character.luck, character.balance, character.alive, character.critical_attack,
-                 character.playability, character.stat_points)
+                 character.playability, character.stat_points, 1, 1)
             )
             new_id = cursor.fetchone()[0]
-            character.id = new_id
-        connection.commit()
-        cursor.close()
-        connection.close()
+            connection.commit()
+            return new_id
+        finally:
+            cursor.close()
+            connection.close()
+            if tunnel is not None:
+                tunnel.stop()
+
+    def _update_stats(self, character):
+        tunnel = self._create_tunnel()
+        tunnel.start()
+        connection = self._create_connection(tunnel)
+        cursor = connection.cursor()
+        try:
+            cursor.execute(
+                "UPDATE characters SET name = %s, level = %s, xp = %s, max_health = %s, health = %s , armor = %s, "
+                "attack = %s, luck = %s, balance = %s, alive = %s, critical_attack = %s, playability = %s, "
+                "stat_points = %s WHERE id = %s",
+                (character.name, character.level, character.xp, character.max_health, character.health, character.armor,
+                 character.attack, character.luck, character.balance, character.alive, character.critical_attack,
+                 character.playability, character.stat_points, character.id)
+            )
+            connection.commit()
+        finally:
+            cursor.close()
+            connection.close()
+            if tunnel is not None:
+                tunnel.stop()
+
+    def add_all(self, characters_list):
+        tunnel = self._create_tunnel()
+        tunnel.start()
+        connection = self._create_connection(tunnel)
+        cursor = connection.cursor()
+        try:
+            for character in characters_list:
+                cursor.execute(
+                    "INSERT INTO characters (name, level, xp, max_health, health, armor, attack, luck, balance, alive, "
+                    "critical_attack, playability, stat_points, owner, status)"
+                    "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) RETURNING id",
+                    (character.name, character.level, character.xp, character.max_health, character.health, character.armor,
+                     character.attack, character.luck, character.balance, character.alive, character.critical_attack,
+                     character.playability, character.stat_points, 1, 1)
+                )
+                new_id = cursor.fetchone()[0]
+                character.id = new_id
+            connection.commit()
+        finally:
+            cursor.close()
+            connection.close()
+            if tunnel is not None:
+                tunnel.stop()
 
     def exist_by_id(self, character_id):
-        connection = self._create_connection()
+        tunnel = self._create_tunnel()
+        tunnel.start()
+        connection = self._create_connection(tunnel)
         cursor = connection.cursor()
-        cursor.execute("SELECT COUNT(*) FROM characters WHERE id = %s", (character_id,))
-        count = cursor.fetchone()[0]
-        cursor.close()
-        connection.close()
-        return count > 0
+        try:
+            cursor.execute("SELECT COUNT(*) FROM characters WHERE id = %s", (character_id,))
+            count = cursor.fetchone()[0]
+            return count > 0
+        finally:
+            cursor.close()
+            connection.close()
+            if tunnel is not None:
+                tunnel.stop()
 
     def find_all(self):
-        connection = self._create_connection()
+        tunnel = self._create_tunnel()
+        tunnel.start()
+        connection = self._create_connection(tunnel)
         cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
-        cursor.execute("SELECT * FROM characters")
-        records = cursor.fetchall()
-        cursor.close()
-        connection.close()
-        return records
+        try:
+            cursor.execute('SELECT * FROM characters')
+            records = cursor.fetchall()
+            return records
+        finally:
+            cursor.close()
+            connection.close()
+            if tunnel is not None:
+                tunnel.stop()
 
     def find_all_by_playability_and_alive(self, playability, alive=True):
-        connection = self._create_connection()
+        tunnel = self._create_tunnel()
+        tunnel.start()
+        connection = self._create_connection(tunnel)
         cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
-        cursor.execute("SELECT * FROM characters "
-                       "WHERE playability = %s "
-                       "AND alive = %s ", (playability, alive))
-        records = cursor.fetchall()
-        cursor.close()
-        connection.close()
-        return records
+        try:
+            cursor.execute("SELECT * FROM characters "
+                           "WHERE playability = %s "
+                           "AND alive = %s ", (playability, alive))
+            records = cursor.fetchall()
+            return records
+        finally:
+            cursor.close()
+            connection.close()
+            if tunnel is not None:
+                tunnel.stop()
 
     def find_all_by_playability_and_alive_and_level(self, playability, level, alive=True):
-        connection = self._create_connection()
+        tunnel = self._create_tunnel()
+        tunnel.start()
+        connection = self._create_connection(tunnel)
         cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
-        cursor.execute("SELECT * FROM characters "
-                       "WHERE level >= 1 AND (level BETWEEN (%s - 1) AND (%s + 1)) "
-                       "AND playability = %s "
-                       "AND alive = %s", (level, level, playability, alive))
-        records = cursor.fetchall()
-        cursor.close()
-        connection.close()
-        return records
+        try:
+            cursor.execute("SELECT * FROM characters "
+                           "WHERE level >= 1 AND (level BETWEEN (%s - 1) AND (%s + 1)) "
+                           "AND playability = %s "
+                           "AND alive = %s", (level, level, playability, alive))
+            records = cursor.fetchall()
+            return records
+        finally:
+            cursor.close()
+            connection.close()
+            if tunnel is not None:
+                tunnel.stop()
 
     def find_by_id(self, character_id):
-        connection = self._create_connection()
+        tunnel = self._create_tunnel()
+        tunnel.start()
+        connection = self._create_connection(tunnel)
         cursor = connection.cursor()
-        cursor.execute("SELECT * FROM characters WHERE id = %s", (character_id,))
-        record = cursor.fetchone()
-        cursor.close()
-        connection.close()
-        return record
+        try:
+            cursor.execute("SELECT * FROM characters WHERE id = %s", (character_id,))
+            record = cursor.fetchone()
+            return record
+        finally:
+            cursor.close()
+            connection.close()
+            if tunnel is not None:
+                tunnel.stop()
 
     def add_character(self, character):
         if character.id is None:
