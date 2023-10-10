fcef677c53dd7b4f21ed19a5d116a4a15c0526a9
BogdanVas <144013490+Ixayerr@users.noreply.github.com>
2023-10-10 18:02:51 +0300

    Update Flask.py
    Incorrect Pep 8 error and non-existent method create_new_mob() have been fixed

--- Flask.py
+++ Flask.py
@@ -3,9 +3,11 @@ from flask_cors import CORS, cross_origin
 import random
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})  # Разрешить доступ только с этого источника
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
+
 
 class Mob:
+
     def __init__(self, name):
         self.name = name
         self.health = random.randint(20, 70)
@@ -15,6 +17,9 @@ class Mob:
         self.critical_attack = self.attack * 2 if self.luck > 5 else self.attack
 
     def to_dict(self):
+        """
+        Преобразует объект моба в словарь.
+        """
         return {
             "name": self.name,
             "critical_attack": self.critical_attack,
@@ -24,6 +29,7 @@ class Mob:
             "luck": self.luck
         }
 
+
 def generate_mobs(num_mobs):
     mob_list = []
     mob_names = ["Zombie", "Skeleton", "Spider", "Slime", "Goblin"]
@@ -32,19 +38,21 @@ def generate_mobs(num_mobs):
         mob_list.append(Mob(name))
     return mob_list
 
+
 @app.route('/')
 def get_mobs():
-    num_mobs = 1  #количество мобов, которое хотите сгенерировать
+    num_mobs = 1
     mobs = generate_mobs(num_mobs)
     mob_data = [mob.to_dict() for mob in mobs]
     return jsonify(mob_data)
 
+
 @app.route('/create', methods=['POST'])
 @cross_origin(origin='http://localhost:3000', headers=['Content-Type', 'Authorization'])
 def create_mob():
-    # Ваш код для создания нового моба здесь
-    new_mob = create_new_mob()
+    new_mob = generate_mobs(1)[0]  # Создать одного нового моба
     return jsonify(new_mob.to_dict())
+
 
 if __name__ == '__main__':
     app.run()
